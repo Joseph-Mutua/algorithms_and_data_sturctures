d5b00f7f15425c6534f949fcefab5cb9f4cc2326
Joseph Mutua <Mutuaj793@gmail.com>
2022-01-16 14:29:34 +0300

    Optimize solution

--- sum_of_integers.js
+++ sum_of_integers.js
@@ -1,16 +1,33 @@
 // Given an array of integers, return the indices of
 // of the two numbers that add up to a given target
 
-const find_sum = (nums, target) => {
-    for(let p1 = 0; p1< nums.length; p1++){
-        const num_to_find = target - nums[p1];
-        for(let p2 = p1+1; p2 < nums.length; p2++ ){
-            if(nums[p2] === num_to_find){
-                 console.log([p1, p2]);
-            }
-        }
+const findTwoSum = (nums, target) => {
+  for (let p1 = 0; p1 < nums.length; p1++) {
+    const num_to_find = target - nums[p1];
+    for (let p2 = p1 + 1; p2 < nums.length; p2++) {
+      if (nums[p2] === num_to_find) {
+        console.log([p1, p2]);
+      }
     }
-    return null;
-}
+  }
+  return null;
+};
 
-find_sum([1, 4, 5, 7, 3, 2, 9],11);
+findTwoSum([1, 4, 5, 7, 3, 2, 9], 11);
+
+// Time-Optimized solution using a hash-map
+const find_two_sum = (nums, target) => {
+  const numsMap = {};
+
+  for (let p = 0; p < nums.length; p++) {
+    const currentMapVal = numsMap[nums[p]];
+    
+    if ((currentMapVal) >= 0) {
+      return [currentMapVal, p];
+    } else {
+      const num_to_find = target - nums[p];
+      numsMap[num_to_find] = p;
+    }
+  }
+  return null;
+};
