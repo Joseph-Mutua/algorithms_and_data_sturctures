c49f58cd59e82165f0bfb17b5b54c953bead1829
Joseph Mutua <Mutuaj793@gmail.com>
2022-01-20 12:11:00 +0300

    Rectify errors

--- linked_lists.js
+++ linked_lists.js
@@ -26,6 +26,32 @@ const reverseLinkedList = (head) => {
 
 // Given a linked list and numbers m and n, return it
 // Back with only positions m to n in reverse
+const reverseBetween = (head, m, n) => {
+  let currentPos = 1,
+    currentNode = head,
+    start = head;
+  while (currentPos < m) {
+    start = currentNode;
+    currentNode = currentNode.next;
+    currentPos++;
+  }
 
+  let newList = null,
+    tail = currentNode;
+  while (currentPos >= m && currentPos <= n) {
+    const next = currentNode.next;
+    currentNode.next = newList;
+    newList = currentNode;
 
+    currentNode = next;
+    currentPos++;
+  }
+  start.next = newList;
+  tail.next = currentNode;
 
+  if (m > 1) {
+    return head;
+  } else {
+    return newList;
+  }
+};
