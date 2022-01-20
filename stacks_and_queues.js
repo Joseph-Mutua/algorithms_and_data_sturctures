c726d9cf97973b8ebda97c490303c1173a46de31
Joseph Mutua <Mutuaj793@gmail.com>
2022-01-20 21:33:10 +0300

    Minimum brackets to make string valid

--- stacks_and_queues.js
+++ stacks_and_queues.js
@@ -24,3 +24,32 @@ const isValidParentheses = (str) => {
   }
   return stack.length === 0;
 };
+
+// Given a string only containing round brackets and "(" and ")"
+// and lowercase characters, remove the least amount of brackets
+// so the string is valid. A string is considered valid if it is
+// empty or if there are brackets, they all close.
+
+const removeBrackets = (str) => {
+  const res = str.split("");
+  const stack = [];
+
+  for (let i = 0; i < res.length; i++) {
+    if (res[i] === "(") {
+      stack.push(res[i]);
+    } else if (res[i] === ")" && stack.length) {
+      stack.pop();
+    } else if (res[i] === ")") {
+      res[i] = "";
+    }
+  }
+
+  while (stack.length) {
+    const currentIndex = stack.pop();
+    res[currentIndex] = "";
+  }
+  return res.join("");
+};
+
+let randStr = "(rggrgfdvfv(f)yth";
+console.log(removeBrackets(randStr));
