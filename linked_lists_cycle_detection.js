43c0c96bc6a6c793cff7c1fa48c847308473bee1
Joseph Mutua <Mutuaj793@gmail.com>
2022-01-20 18:16:01 +0300

    Floyd's hare and tortoise algorithm

--- linked_lists_cycle_detection.js
+++ linked_lists_cycle_detection.js
@@ -11,3 +11,31 @@ const findCycle = (head) => {
   }
   return currentNode;
 };
+
+// Floyd's Hare and Tortoise Algorithm
+const findCycle = (head) => {
+  let hare = head,
+    tortoise = head;
+
+  while (true) {
+    hare = hare.next;
+    tortoise = tortoise.next;
+
+    if (hare === null || tortoise === null) {
+      return false;
+    } else {
+      hare = hare.next;
+    }
+
+    if (tortoise === hare) break;
+  }
+  
+  let p1 = head,
+    p2 = tortoise;
+
+  while (p1 !== p2) {
+    p1 = p1.next;
+    p2 = p2.next;
+  }
+  return p1;
+};
