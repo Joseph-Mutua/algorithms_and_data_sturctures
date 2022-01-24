ad2d40c05bc20495ca85a3f5d7caa168cf05c6e6
Joseph Mutua <Mutuaj793@gmail.com>
2022-01-24 20:07:59 +0300

    Insertion sort

--- sorting.js
+++ sorting.js
@@ -21,8 +21,56 @@ function bubbleSort(array) {
       }
     }
   }
+  return array;
 }
 
-console.log(numbers);
-bubbleSort(numbers)
-console.log(numbers);
+// console.log(numbers);
+// bubbleSort(numbers)
+// console.log(numbers);
+
+// Selection Sort
+function selectionSort(array) {
+  const length = array.length;
+
+  for (let i = 0; i < length; i++) {
+    // Set current index as minimum
+
+    let min = i;
+    let temp = array[i];
+
+    for (let j = i + 1; j < length; j++) {
+      if (array[j] < array[min]) {
+        // update minimum if current is lower
+        // than what we had previously
+        min = j;
+      }
+    }
+    array[i] = array[min];
+    array[min] = temp;
+  }
+  return array;
+}
+
+// selectionSort(numbers);
+// console.log(numbers);
+
+// Insertion Sort
+function insertionSort() {
+  const length = array.length;
+
+  for (let i = 0; i < length; i++) {
+    if (array[i] < array[0]) {
+      // move member to the first position
+
+      array.unshift(array.splice(i, 1)[0]);
+    } else {
+      // find where number should go
+      for (let j = 1; j < i; j++) {
+        if (array[i] > array[j - 1] && array[i] < array[j]) {
+          // move number to the right spot
+          array.splice(j, 0, array.splice(i, 1)[0]);
+        }
+      }
+    }
+  }
+}
