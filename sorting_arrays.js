8e1bae5cc45153225264d27de7e70ed4b901d52d
Joseph Mutua <Mutuaj793@gmail.com>
2022-01-28 19:04:58 +0300

    Searching range of occurence

--- sorting_arrays.js
+++ sorting_arrays.js
@@ -78,7 +78,7 @@ const quickSelect = (array, left, right, indexToFind) => {
   if (left < right) {
     const partitionIndex = partition(array, left, right);
 
-    if ((partitonIndex = indexToFind)) {
+    if ((partitionIndex = indexToFind)) {
       return array[partitionIndex];
     } else if (indexToFind < partitionIndex) {
       return quickSelect(array, left, partitionIndex - 1, indexToFind);
@@ -114,3 +114,72 @@ const getKthElement = (array, k) => {
   quickSort(array, 0, array.length - 1);
   return array[indexToFind];
 };
+
+// BINARY SEARCH
+const binarySearch = (array, target) => {
+  let left = 0,
+    right = array.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    const foundVal = array[mid];
+
+    if (foundVal === target) {
+      return mid;
+    } else if (foundVal < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+};
+
+// Given an array of integers sorted in ascending order,return the
+// starting and ending index of a given value in an array, i.e [x,y]
+// Your solution should end in O(log N) time
+const binarySearch2 = (nums, left, right, target) => {
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    const foundVal = nums[mid];
+
+    if (foundVal === target) {
+      return mid;
+    } else if (foundVal < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+};
+
+const searchRange = (nums, target) => {
+  if (nums.length === 0) return [-1, -1];
+
+  const firstPos = binarySearch2(nums, 0, nums.length - 1, target);
+  if (firstPos === -1) return [-1, -1];
+
+  let startPos = firstPos,
+    endPos = firstPos,
+    temp1,
+    temp2;
+
+  while (startPos !== -1) {
+    temp1 = startPos;
+    startPos = binarySearch2(nums, 0, startPos - 1, target);
+  }
+  startPos = temp1;
+  while (endPos !== -1) {
+    temp2 = endPos;
+    endPos = binarySearch2(nums, endPos + 1, nums.length - 1, target);
+  }
+  endPos = temp2;
+  return [startPos, endPos];
+};
+
+
+let nums =[1,2,3,3,5,5,5,5,8,9,9]
+quickSearch(nums, 2)
