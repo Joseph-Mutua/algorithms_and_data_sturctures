28e913ed601a7cf9ac73fbdab5b4ca379985b6d4
Joseph Mutua <Mutuaj793@gmail.com>
2022-01-31 12:29:59 +0300

    BFS vs DFS

--- trees.js
+++ trees.js
@@ -25,3 +25,43 @@ function BinaryTreeNode(val) {
 
 // Cons of Binary Search Trees
 // 1. No O(1) Operations
+
+// Implementation
+class Node {}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {}
+  lookup(value) {}
+  remove(value) {}
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(150);
+tree.insert(75);
+tree.insert(1);
+
+
+// BFS VS DFS
+
+// BFS                      DFS
+// PROS
+// Shortest path            Less memory
+// Closer nodes             Does path exist
+
+// CONS
+// More memory              Can be slow
+
+// If you know  a solution is not far from root of tree: BFS
+// If tree is very deep and solutions are rare: BFS --> DFS WILL TAKE LONG
+// If the tree is very wide: DFS --> BFS will take lots of memory
+// If solutions are frequent but located deep in the tree: DFS
+// Determining whether a path exists between two nodes: DFS
+// Finding the shortest path: BFS
