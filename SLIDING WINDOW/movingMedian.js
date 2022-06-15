febd8fd22efd30973f633987d73eb8da3c63737b
Joseph Mutua <Mutuaj793@gmail.com>
2022-06-15 23:53:03 +0300

    Carry out test cases

--- SLIDING WINDOW/movingMedian.js
+++ SLIDING WINDOW/movingMedian.js
@@ -16,3 +16,31 @@
 
 // Input: [3, 0, 0, -2, 0, 2, 0, -2];
 // Output: 0, 0, 0, 0, 0, 0, 0;
+
+function ArrayChallenge(arr) {
+  // code goes here
+  function getMedian(arr) {
+    arr.sort((a, b) => a - b);
+    let len = arr.length;
+
+    if (len % 2) {
+      return arr[Math.floor(len / 2)];
+    } else {
+      return (arr[len / 2 - 1] + arr[len / 2]) / 2;
+    }
+  }
+
+  let buildArr = [];
+  let winLength = arr.shift();
+  let len = arr.length;
+
+  for (let i = 0; i < len; i++) {
+    let firstIndex = Math.max(0, i - winLength + 1);
+    let med = getMedian(arr.slice(firstIndex, i + 1));
+    buildArr.push(med);
+  }
+  return buildArr.join(",");
+}
+
+// keep this function call here
+console.log(ArrayChallenge([5, 2,4,6]));
