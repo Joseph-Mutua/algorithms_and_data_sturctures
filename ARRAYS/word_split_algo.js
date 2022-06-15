d8785266144cd689e9ba8e1c1ceb3d0f6fe3805d
Joseph Mutua <Mutuaj793@gmail.com>
2022-06-16 00:12:33 +0300

    Finish test cases

--- ARRAYS/word_split_algo.js
+++ ARRAYS/word_split_algo.js
@@ -1,4 +1,4 @@
-Problem: //Have the function WordSplit(strArr) read the array of strings stored in strArr,
+//Have the function WordSplit(strArr) read the array of strings stored in strArr,
 // which will contain 2 elements: the first element will be a sequence of characters, 
 //and the second element will be a long string of comma-seperated words, in alphabetical order, 
 //that represents a dictionary of some arbitrary length. For example:
@@ -11,3 +11,30 @@ Problem: //Have the function WordSplit(strArr) read the array of strings stored 
 //There will only be one correct way to split the first element of characters into two words. If there is no way
 // to split string into two words that exist in the dictionary, return the string not possible. 
 //The first element itself will never exist in the dictionary as a real word.
+
+function ArrayChallenge(strArr) {
+  // code goes here
+  let wordToCompare = strArr[0];
+  let stringDictionary = strArr[1];
+
+  let singleStrings = stringDictionary.split(",");
+
+  let answerWords = "";
+
+  singleStrings.map((firstWord) => {
+    let splitMainWordArray = wordToCompare.split(firstWord);
+    if (splitMainWordArray.length > 0) {
+      splitMainWordArray.map((word) => {
+        let joinedWord = firstWord + word;
+        let reversedWord = [joinedWord].reverse().toString();
+
+        if (joinedWord === wordToCompare || reversedWord === wordToCompare) {
+          answerWords = "" + firstWord + "," + word + "";
+        } else {
+          return "Not Possible";
+        }
+      });
+    }
+  });
+  return answerWords;
+}
